f455eb8c6ebdbd9eaf919acf40b552eab9878026
ManishJoc14 <manishjoc14@gmail.com>
2024-02-10 21:32:01 +0545

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./app.css";
+import music from "./music/bg.mp3";
+import { answers } from "./answers/answers";
 
 function App() {
+  const [ans, setAns] = useState(false);
+  const [isNoClicked, setIsNoClicked] = useState(false);
+  const [count, setCount] = useState(1);
+  const [audio] = useState(new Audio(music));
+  const setYes = () => {
+    setAns(true);
+    setCount(1);
+    const elm = document.getElementsByClassName("yesBtn")[0];
+    elm.style.heigth = "60px";
+    elm.style.width = "100px";
+    setIsNoClicked(false);
+    playAudio();
+  };
+
+  const setNo = () => {
+    setAns(false);
+    setIsNoClicked(true);
+    setCount((prev) => {
+      if (prev <= 14) {
+        return prev + 1;
+      } else {
+        return 1;
+      }
+    });
+
+    playAudio();
+  };
+  const playAudio = () => {
+    audio.play().catch((error) => {
+      console.error("Failed to play audio:", error);
+    });
+  };
+  document.addEventListener("click", playAudio);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className="myQN">
+          <img
+            src={
+              ans
+                ? require("./images/yes.gif")
+                : require("./images/willyoubemyvalentine.gif")
+            }
+            alt=""
+          />
+        </div>
+
+        {ans ? (
+          <p className="myquestion" style={{ fontSize: "40px", textAlign: 'center' }}>
+            Yepppie, see you sooonnn 😘{" "}
+          </p>
+        ) : (
+          <p className="myquestion">{answers[count - 1]}</p>
+        )}
+        <div className="buttons">
+          <button
+            className="yesBtn"
+            onClick={setYes}
+            style={{
+              height: isNoClicked ? ` ${60 * count}px` : "60px",
+              width: isNoClicked ? ` ${100 * count}px` : "100px",
+            }}
+          >
+            Yes
+          </button>
+          <button className="noBtn" onClick={setNo} >
+            No
+          </button>
+        </div>
+
+        <p className="footer">Develop by Manish with ❤️</p>
+      </div>
+    </>
   );
 }
 
---
+++ src/answers/answers.js
@@ -0,0 +1,17 @@
+export const answers = [
+  "  Will you be my valentine ? ",
+    "Are you sure?",
+    "Are you really sure??",
+    "Are you really realy sure???",
+    "Think again?",
+    "Don't believe in second chances?",
+    "Why are you being so cold?",
+    "Maybe we can talk about it?",
+    "I am not going to ask again!",
+    "Ok now this is hurting my feelings!",
+    "You are now just being mean!",
+    "Why are you doing this to me?",
+    "Please give me a chance!",
+    "I am begging you to stop!",
+    "Ok, Lets just start over.."
+]
